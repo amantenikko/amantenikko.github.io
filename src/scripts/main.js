5a680eda031348375f0e0be43179390d0e75b5ff
Nikko Amante <84558605+amantenikko@users.noreply.github.com>
2022-09-18 15:19:27 +0800

    update contents

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+
 // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 $(function(){
@@ -73,46 +76,96 @@ window.addEventListener('load', function() {
     /* ---- particles.js config ---- */
     // particlesJS.load('particles-js', 'particles-config.json');
 
+    const canvas = document.querySelector("#particles-js");
+    const backgroundColor = "#FFF";
+    let model;
     const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight
+        width: canvas.offsetWidth,
+        height: canvas.offsetHeight
     }
-    
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(backgroundColor);
+
     const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+    camera.lookAt( scene.position );
+    camera.position.set(0, 3, 15);
+    
+    const loader = new GLTFLoader();
+    loader.load( '/src/NA.glb', function ( gltf ) {
+        model = gltf.scene;
+        model.scale.set(1, 1, 1);
+        model.position.x = 5;
+        
+        scene.add( model );
+    },undefined, function (error) {
+        console.error(error);
+    });
+    
+    let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+    hemiLight.position.set(0, 50, 0);
+    scene.add(hemiLight);
+    
+    let d = 8.25;
+    let dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+    dirLight.position.set(-8, 12, 8);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 1500;
+    dirLight.shadow.camera.left = d * -1;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = d * -1;
+    scene.add(dirLight);
+    
+    let floorGeo = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+    let floorMat = new THREE.MeshPhongMaterial({
+        color: 0xeeeeee,
+      shininess: 0
+    });
+    let floor = new THREE.Mesh(floorGeo, floorMat);
+    floor.rotation.x = -0.5 * Math.PI;
+    floor.receiveShadow = true;
+    floor.position.y = -11;
+    scene.add(floor);
     
     const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize( sizes.width, sizes.height );
-    document.querySelector('#particles-js').appendChild( renderer.domElement );
-    
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    
-    camera.position.z = 3;
+    canvas.appendChild( renderer.domElement );
     renderer.render( scene, camera );
+
     (function animate() {
+
         requestAnimationFrame( animate );
-        cube.rotation.y += 0.02;
         renderer.render( scene, camera );
-    })(); // animate();
+
+        if (model) model.rotation.y -= 0.005;
+    })();
 
     window.addEventListener('resize', () =>
-    {
-    // Update sizes
-    sizes.width = window.innerWidth
-    // sizes.height = window.innerHeight
+    {   
+        model.position.x = (canvas.offsetWidth < 1000)? 0: 5;
+        sizes.width = canvas.offsetWidth;
+        sizes.height = canvas.offsetHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+        // Update camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     })
+
+    document.addEventListener("mousemove", function (e) {
+        let mousecoords = getMousePos(e);
+        // console.log(mousecoords.y);
+    });
+    function getMousePos(e) {
+        return { x: e.clientX, y: e.clientY };
+    };
 });
+
 
 // gsap.set(".cursor", {xPercent: -50, yPercent: -50});
 
